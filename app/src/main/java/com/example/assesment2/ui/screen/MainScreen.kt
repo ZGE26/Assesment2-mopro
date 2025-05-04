a751552965e59fa64e0f1c85cc4203cf13376fb0
ARYA ERSI PUTRA <aryaersiputra8@gmail.com>
2025-05-04 16:49:32 +0700

    Update FBA Expandable dan Membuat Tombol Ubah Tampilan List

--- app/src/main/java/com/example/assesment2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/assesment2/ui/screen/MainScreen.kt
@@ -21,10 +21,23 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.assesment2.R
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.painterResource
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    var showList by remember { mutableStateOf(true) }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -39,16 +52,49 @@ fun MainScreen() {
             )
         },
         floatingActionButton = {
-            FloatingActionButton(
-                onClick = {}
+            var expanded by remember { mutableStateOf(false) }
+
+            Box(
+                contentAlignment = Alignment.BottomEnd
             ) {
-                Icon(
-                    imageVector = Icons.Filled.Add,
-                    contentDescription = "Tambah Catatan",//Ganti sama string resource nanti
-                    tint = MaterialTheme.colorScheme.primary
-                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    horizontalAlignment = Alignment.End
+                ) {
+                    if (expanded) {
+                        FloatingActionButton(
+                            onClick = { showList = !showList},
+                            containerColor = MaterialTheme.colorScheme.secondary
+                        ) {
+                            Icon(
+                                painter = painterResource(
+                                    if (showList) R.drawable.baseline_grid_view_24
+                                    else R.drawable.baseline_format_list_bulleted_24
+                                ),
+                                contentDescription = "Tambah Reminder")
+                        }
+                        FloatingActionButton(
+                            onClick = { /* Tambah Catatan */ },
+                            containerColor = MaterialTheme.colorScheme.secondary
+                        ) {
+                            Icon(Icons.Filled.Create, contentDescription = "Tambah Catatan")
+                        }
+                    }
+
+                    FloatingActionButton(
+                        onClick = { expanded = !expanded },
+                        containerColor = MaterialTheme.colorScheme.primaryContainer
+                    ) {
+                        Icon(
+                            imageVector = if (expanded) Icons.Filled.Close else Icons.Filled.Add,
+                            contentDescription = "Menu Tambah",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
             }
         }
+
     ) { innerPadding->
         ScreenContent(Modifier.padding(innerPadding))
     }
