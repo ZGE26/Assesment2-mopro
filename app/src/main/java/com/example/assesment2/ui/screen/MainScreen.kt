e4431dee31d866760b34e01d8ae1d7edc88a266a
ARYA ERSI PUTRA <aryaersiputra8@gmail.com>
2025-05-06 07:34:31 +0700

    Animasi FBA dan Buat TaskJudulDialog

--- app/src/main/java/com/example/assesment2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/assesment2/ui/screen/MainScreen.kt
@@ -1,42 +1,31 @@
 package com.example.assesment2.ui.screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.assesment2.R
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Create
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.res.painterResource
-
+import com.example.assesment2.ui.component.TaskJudulDialog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
     var showList by remember { mutableStateOf(true) }
+    var showDialog by remember { mutableStateOf(false) } // hanya satu di sini
 
     Scaffold(
         topBar = {
@@ -44,26 +33,29 @@ fun MainScreen() {
                 title = {
                     Text(stringResource(R.string.app_name))
                 },
-                colors =TopAppBarDefaults.mediumTopAppBarColors(
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
-
                 )
             )
         },
         floatingActionButton = {
             var expanded by remember { mutableStateOf(false) }
-
             Box(
-                contentAlignment = Alignment.BottomEnd
+                contentAlignment = Alignment.BottomEnd,
+                modifier = Modifier.padding(end = 16.dp, bottom = 16.dp)
             ) {
                 Column(
                     verticalArrangement = Arrangement.spacedBy(12.dp),
-                    horizontalAlignment = Alignment.End
+                    horizontalAlignment = Alignment.End,
                 ) {
-                    if (expanded) {
+                    AnimatedVisibility(
+                        visible = expanded,
+                        enter = fadeIn() + expandVertically(),
+                        exit = fadeOut() + shrinkVertically()
+                    ) {
                         FloatingActionButton(
-                            onClick = { showList = !showList},
+                            onClick = { showList = !showList },
                             containerColor = MaterialTheme.colorScheme.secondary
                         ) {
                             Icon(
@@ -71,15 +63,24 @@ fun MainScreen() {
                                     if (showList) R.drawable.baseline_grid_view_24
                                     else R.drawable.baseline_format_list_bulleted_24
                                 ),
-                                contentDescription = "Tambah Reminder")
+                                contentDescription = "Tukar Tampilan"
+                            )
                         }
+                    }
+
+                    AnimatedVisibility(
+                        visible = expanded,
+                        enter = fadeIn() + expandVertically(),
+                        exit = fadeOut() + shrinkVertically()
+                    ) {
                         FloatingActionButton(
-                            onClick = { /* Tambah Catatan */ },
+                            onClick = { showDialog = true },
                             containerColor = MaterialTheme.colorScheme.secondary
                         ) {
                             Icon(Icons.Filled.Create, contentDescription = "Tambah Catatan")
                         }
                     }
+
 
                     FloatingActionButton(
                         onClick = { expanded = !expanded },
@@ -94,18 +95,24 @@ fun MainScreen() {
                 }
             }
         }
-
-    ) { innerPadding->
+    ) { innerPadding ->
         ScreenContent(Modifier.padding(innerPadding))
+
+        if (showDialog) {
+            TaskJudulDialog(
+                onDismissRequest = { showDialog = false },
+                onConfirmation = {}
+            )
+        }
     }
 }
 
 @Composable
-fun ScreenContent(
-    modifier: Modifier
-) {
+fun ScreenContent(modifier: Modifier) {
     Column(
-        modifier = modifier.fillMaxSize().padding(8.dp)
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
     ) {
         Text("Testing")
     }
