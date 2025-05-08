87a7096837d2713ba9e1420838f6855580f4b718
ARYA ERSI PUTRA <aryaersiputra8@gmail.com>
2025-05-08 16:18:02 +0700

    Menyimpan preference pengguna

--- app/src/main/java/com/example/assesment2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/example/assesment2/ui/screen/MainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -17,12 +18,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
@@ -57,16 +64,22 @@ import com.example.assesment2.R
 import com.example.assesment2.model.TaskHalder
 import com.example.assesment2.navigation.Screen
 import com.example.assesment2.ui.component.CreateTask
+import com.example.assesment2.util.SettingsDataStore
 import com.example.assesment2.util.ViewModelFactory
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(navController: NavHostController) {
-    var showList by remember { mutableStateOf(true) }
     var showDialog by remember { mutableStateOf(false) }
 
     val context = LocalContext.current
+    val dataStore = SettingsDataStore(context)
+    val showList by dataStore.layoutFlow.collectAsState(initial = true)
+
     val factory = ViewModelFactory(context)
     val viewModel: MainViewModel = viewModel(factory = factory)
     val data by viewModel.halder.collectAsState()
@@ -74,9 +87,7 @@ fun MainScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = {
-                    Text(stringResource(R.string.app_name))
-                },
+                title = { Text(stringResource(R.string.app_name)) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
                     titleContentColor = MaterialTheme.colorScheme.primary
@@ -99,7 +110,11 @@ fun MainScreen(navController: NavHostController) {
                         exit = fadeOut() + shrinkVertically()
                     ) {
                         FloatingActionButton(
-                            onClick = { showList = !showList },
+                            onClick = {
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    dataStore.saveLayout(!showList)
+                                }
+                            },
                             containerColor = MaterialTheme.colorScheme.secondary
                         ) {
                             Icon(
@@ -139,8 +154,7 @@ fun MainScreen(navController: NavHostController) {
             }
         }
     ) { innerPadding ->
-        // Update ScreenContent with data
-        ScreenContent(Modifier.padding(innerPadding), navController, data)
+        ScreenContent(showList, Modifier.padding(innerPadding), navController, data)
 
         if (showDialog) {
             CreateTask(
@@ -154,17 +168,20 @@ fun MainScreen(navController: NavHostController) {
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier, navController: NavHostController, data: List<TaskHalder> = emptyList()) {
+fun ScreenContent(
+    showList: Boolean,
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    data: List<TaskHalder>
+) {
     var searchQuery by remember { mutableStateOf("") }
 
-    // Filter data based on search query
     val filteredData = data.filter {
         it.title.contains(searchQuery, ignoreCase = true) ||
                 it.category.contains(searchQuery, ignoreCase = true)
     }
 
-    Column(modifier = modifier.padding(16.dp)) {
-        // Search Input Field
+    Column(modifier = modifier.fillMaxSize()) {
         OutlinedTextField(
             value = searchQuery,
             onValueChange = { searchQuery = it },
@@ -179,37 +196,51 @@ fun ScreenContent(modifier: Modifier, navController: NavHostController, data: Li
             shape = RoundedCornerShape(percent = 50),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 8.dp),
+                .padding(horizontal = 16.dp, vertical = 8.dp),
             keyboardOptions = KeyboardOptions(
                 capitalization = KeyboardCapitalization.Sentences,
                 imeAction = ImeAction.Done
             )
         )
 
-        // Display message if no data is found
         if (filteredData.isEmpty()) {
-            Column(
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(top = 32.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
             ) {
                 Text(stringResource(R.string.invalid_data))
             }
         } else {
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                contentPadding = PaddingValues(bottom = 84.dp)
-            ) {
-                items(filteredData) {
-                    ListTask(
-                        taskHalder = it,
-                        onClick = { navController.navigate(Screen.ListTask.passHalderId(it.id)) }
-                    )
-                    HorizontalDivider()
+            if (showList) {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(bottom = 84.dp)
+                ) {
+                    items(filteredData) {
+                        ListTask(
+                            taskHalder = it,
+                            onClick = { navController.navigate(Screen.ListTask.passHalderId(it.id)) }
+                        )
+                        HorizontalDivider()
+                    }
+                }
+            } else {
+                LazyVerticalStaggeredGrid(
+                    modifier = Modifier.fillMaxSize(),
+                    columns = StaggeredGridCells.Fixed(2),
+                    verticalItemSpacing = 8.dp,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(8.dp, bottom = 84.dp)
+                ) {
+                    items(filteredData) {
+                        GridItem(
+                            taskHalder = it,
+                            onClick = { navController.navigate(Screen.ListTask.passHalderId(it.id)) }
+                        )
+                    }
                 }
             }
         }
@@ -244,6 +275,34 @@ fun ListTask(taskHalder: TaskHalder, onClick: () -> Unit = {}) {
                 fontWeight = FontWeight.Bold
             )
             Text(taskHalder.category)
+        }
+    }
+}
+
+@Composable
+fun GridItem(taskHalder: TaskHalder, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier.fillMaxWidth().clickable { onClick() },
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+        ),
+        border = BorderStroke(1.dp, DividerDefaults.color)
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            Text(
+                text = taskHalder.title,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = taskHalder.category,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
         }
     }
 }
